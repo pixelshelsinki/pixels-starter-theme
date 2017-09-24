9e6b05fc586efae61b6629f0f403bdcc5977c184
Thomas Hurd <thomas@pixels.fi>
2017-09-24 15:39:34 +0300

    update for i18n

--- functions.php
+++ functions.php
@@ -1,41 +1,53 @@
 <?php
+/**
+ * Loads theme function files.
+ *
+ */
 
 /**
- * Helper function for prettying up errors TODO: clean up.
+ * Load translation for the theme.
+ */
+function starter_theme_load_theme_textdomain() {
+  load_theme_textdomain( 'pixels-text-domain', get_template_directory() . '/languages' );
+}
+add_action( 'after_setup_theme', 'starter_theme_load_theme_textdomain' );
+
+/**
+ * Helper function for prettying up errors
  * @param string $message
  * @param string $subtitle
  * @param string $title
  */
 $pixels_error = function ($message, $subtitle = '', $title = '') {
-    $title = $title ?: __('Theme &rsaquo; Error', 'pixels-text-domain');
-    $footer = __('You can report this error to <a href="mailto:support@pixels.fi">support@pixels.fi</a>', 'pixels-text-domain');
-    $message = "<h1>{$title}<br><small>{$subtitle}</small></h1><p>{$message}</p><p>{$footer}</p>";
-    wp_die($message, $title);
+  $title = $title ?: __('Theme &rsaquo; Error', 'pixels-text-domain');
+  $footer = __('You can report this error to <a href="mailto:support@pixels.fi">support@pixels.fi</a>', 'pixels-text-domain');
+  $message = "<h1>{$title}<br><small>{$subtitle}</small></h1><p>{$message}</p><p>{$footer}</p>";
+  wp_die($message, $title);
 };
 
 /**
  * Ensure compatible version of PHP is used
  */
 if (version_compare('5.6.4', phpversion(), '>=')) {
-    $pixels_error(__('You must be using PHP 5.6.4 or greater.', 'pixels-text-domain'), __('Invalid PHP version', 'pixels-text-domain'));
+  $pixels_error(__('You must be using PHP 5.6.4 or greater.', 'pixels-text-domain'), __('Invalid PHP version', 'pixels-text-domain'));
 }
 
 /**
  * Ensure compatible version of WordPress is used
  */
 if (version_compare('4.7.0', get_bloginfo('version'), '>=')) {
-    $pixels_error(__('You must be using WordPress 4.7.0 or greater.', 'pixels-text-domain'), __('Invalid WordPress version', 'pixels-text-domain'));
+  $pixels_error(__('You must be using WordPress 4.7.0 or greater.', 'pixels-text-domain'), __('Invalid WordPress version', 'pixels-text-domain'));
 }
 
 /**
- * Sage required files
+ * Theme required files
  *
  * The mapped array determines the code library included in your theme.
  * Add or remove files to the array as needed. Supports child theme overrides.
  */
 array_map(function ($file) use ($pixels_error) {
-    $file = "lib/{$file}.php";
-    if (!locate_template($file, true, true)) {
-        $pixels_error(sprintf(__('Error locating <code>%s</code> for inclusion.', 'pixels-text-domain'), $file), 'File not found');
-    }
+  $file = "lib/{$file}.php";
+  if (!locate_template($file, true, true)) {
+    $pixels_error(sprintf(__('Error locating <code>%s</code> for inclusion.', 'pixels-text-domain'), $file), 'File not found');
+  }
 }, ['assets', 'timber', 'widget-areas', 'filters']);
--- lib/filters.php
+++ lib/filters.php
@@ -6,12 +6,12 @@ namespace Theme\Filters;
  * Add <body> classes
  */
 add_filter('body_class', function (array $classes) {
-    /** Add page slug if it doesn't exist */
-    if (is_single() || is_page() && !is_front_page()) {
-        if (!in_array(basename(get_permalink()), $classes)) {
-            $classes[] = basename(get_permalink());
-        }
+  /** Add page slug if it doesn't exist */
+  if (is_single() || is_page() && !is_front_page()) {
+    if (!in_array(basename(get_permalink()), $classes)) {
+      $classes[] = basename(get_permalink());
     }
+  }
 
-    return array_filter($classes);
+  return array_filter($classes);
 });
