9b32ea50e3bb75434ed47922304deb70c4d49a52
Thomas Hurd <thomas@pixels.fi>
2019-07-31 14:10:24 +0300

    fix styling issues

--- lib/templates.php
+++ lib/templates.php
@@ -1,37 +1,49 @@
 <?php
 /**
- * mods templates
+ * Template Loading.
+ *
+ * This file modifies how templates are found and loaded.
+ *
+ * @package WordPress
+ * @subpackage PixelsTheme
  */
 
 namespace PixelsTheme\Templates;
 
+/**
+ * Intercepts the template hierarchy and moves the expected location to the
+ * `data/` directory.
+ *
+ * @param  array $templates The templates for the requested type.
+ * @return array            The modified templates.
+ */
 function intercept_template_hierarchy( $templates ) {
-  if ( file_exists( get_theme_file_path() . '/data/' ) ) {
-    $templates = preg_filter('/^/', 'data/', $templates);
-  }
+	if ( file_exists( get_theme_file_path() . '/data/' ) ) {
+		$templates = preg_filter( '/^/', 'data/', $templates );
+	}
 
-  return $templates;
+	return $templates;
 }
 
 $wp_types = [
-  'index',
-  '404',
-  'archive',
-  'author',
-  'category',
-  'tag',
-  'taxonomy',
-  'date',
-  'home',
-  'frontpage',
-  'page',
-  'paged',
-  'search',
-  'single',
-  'singular',
-  'attachment',
+	'index',
+	'404',
+	'archive',
+	'author',
+	'category',
+	'tag',
+	'taxonomy',
+	'date',
+	'home',
+	'frontpage',
+	'page',
+	'paged',
+	'search',
+	'single',
+	'singular',
+	'attachment',
 ];
 
-foreach ( $wp_types as $type ) {
-  add_filter( "{$type}_template_hierarchy", __NAMESPACE__ . '\\intercept_template_hierarchy' );
+foreach ( $wp_types as $wp_type ) {
+	add_filter( "{$wp_type}_template_hierarchy", __NAMESPACE__ . '\\intercept_template_hierarchy' );
 }
