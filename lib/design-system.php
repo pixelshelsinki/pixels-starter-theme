f42a4936d71ab1dc89e2a9282d5f1e4b7d3bd612
Thomas Hurd <thomas@pixels.fi>
2019-08-12 15:02:04 +0300

    update editor config, fix up lib/design-system.php to pass tests

--- lib/design-system.php
+++ lib/design-system.php
@@ -1,35 +1,45 @@
 <?php
+/**
+ * Functions for setting up the design system.
+ *
+ * @package WordPress
+ * @subpackage PixelsTheme
+ */
 
 namespace PixelsTheme\DesignSystem;
 
 /**
- * Add the `component` query variable so Wordpress won't mangle them.
+ * Add the `component` query variable so WordPress won't mangle them.
+ *
+ * @param array $vars The query vars.
  */
-function add_query_vars( $vars ){
-  $vars[] = 'component';
+function add_query_vars( $vars ) {
+	$vars[] = 'component';
 
-  return $vars;
+	return $vars;
 }
-add_filter( 'query_vars', __NAMESPACE__ . '\\add_query_vars');
+add_filter( 'query_vars', __NAMESPACE__ . '\\add_query_vars' );
 
 /**
  * Adds a rewrite for design system sections.
  *
- * @return [type] [description]
+ * @return void
  */
 function custom_rewrite_basic() {
-  add_rewrite_rule('^design-system/([a-zA-Z0-9]+)/?', 'design-system?component=$1', 'top');
+	add_rewrite_rule( '^design-system/([a-zA-Z0-9]+)/?', 'design-system?component=$1', 'top' );
 }
-// add_action('init', __NAMESPACE__ . '\\custom_rewrite_basic');
+// "add_action('init', __NAMESPACE__ . '\\custom_rewrite_basic');".
 
 
 /**
  * Adds the endpoints to URLs for ds-section.
+ *
+ * @return void
  */
 function add_url_endpoints() {
-  add_rewrite_endpoint( 'component', EP_ALL );
+	add_rewrite_endpoint( 'component', EP_ALL );
 }
-// add_action( 'init', __NAMESPACE__ . '\\add_url_endpoints' );
+// "add_action( 'init', __NAMESPACE__ . '\\add_url_endpoints' );".
 
 /**
  * Gets the component manifest for the design system.
@@ -37,22 +47,22 @@ function add_url_endpoints() {
  * @return array Array of all components.
  */
 function get_component_manifest() {
-  $component_manifest = [];
+	$component_manifest = [];
 
-  // Get a list of all top level directories in the components folder.
-  $component_directories = glob( get_template_directory() . '/components/*' );
+	// Get a list of all top level directories in the components folder.
+	$component_directories = glob( get_template_directory() . '/components/*' );
 
-  foreach ( $component_directories as $key => $component_directory_path ) {
-    $component_slug = basename( $component_directory_path );
+	foreach ( $component_directories as $key => $component_directory_path ) {
+		$component_slug = basename( $component_directory_path );
 
-    $component_manifest[$component_slug] = [
-      'slug' => $component_slug,
-      'label' => ucwords( str_replace( '-', ' ', $component_slug ) ),
-      'path' => $component_directory_path,
-    ];
-  }
+		$component_manifest[ $component_slug ] = [
+			'slug'  => $component_slug,
+			'label' => ucwords( str_replace( '-', ' ', $component_slug ) ),
+			'path'  => $component_directory_path,
+		];
+	}
 
-  return $component_manifest;
+	return $component_manifest;
 }
 
 /**
@@ -62,20 +72,20 @@ function get_component_manifest() {
  * @return array                  The navigation items.
  */
 function get_navigation( $current_component = '' ) {
-  $nav_menu_items = [];
+	$nav_menu_items = [];
 
-  $component_manifest = get_component_manifest();
+	$component_manifest = get_component_manifest();
 
-  foreach ( $component_manifest as $key => $component ) {
-    $nav_menu_items[] = [
-      'label' => $component['label'],
-      'slug' => $component['slug'],
-      'link' => get_navigation_link( $component['slug'] ),
-      'active' => ( $current_component === $component['slug'] ),
-    ];
-  }
+	foreach ( $component_manifest as $key => $component ) {
+		$nav_menu_items[] = [
+			'label'  => $component['label'],
+			'slug'   => $component['slug'],
+			'link'   => get_navigation_link( $component['slug'] ),
+			'active' => ( $current_component === $component['slug'] ),
+		];
+	}
 
-  return $nav_menu_items;
+	return $nav_menu_items;
 }
 
 /**
@@ -85,7 +95,7 @@ function get_navigation( $current_component = '' ) {
  * @return string                 The url to the component in the design system.
  */
 function get_navigation_link( $component_slug ) {
-  return get_the_permalink() . '?component=' . $component_slug;
+	return get_the_permalink() . '?component=' . $component_slug;
 }
 
 /**
@@ -95,20 +105,19 @@ function get_navigation_link( $component_slug ) {
  * @return array                  The component data.
  */
 function get_component( $component_slug ) {
-  $component_manifest = get_component_manifest();
+	$component_manifest = get_component_manifest();
 
-  // Get the component we need from the manifest
-  $component = isset( $component_manifest[$component_slug] ) ? $component_manifest[$component_slug] : false;
+	// Get the component we need from the manifest.
+	$component = isset( $component_manifest[ $component_slug ] ) ? $component_manifest[ $component_slug ] : false;
 
-  if ( ! $component ) {
-    return [];
-  }
+	if ( ! $component ) {
+		return [];
+	}
 
-  $component['description'] = get_component_description( $component_slug );
+	$component['description'] = get_component_description( $component_slug );
+	$component['variations']  = get_component_variations( $component_slug );
 
-  $component['variations'] = get_component_variations( $component_slug );
-
-  return $component;
+	return $component;
 }
 
 /**
@@ -118,25 +127,25 @@ function get_component( $component_slug ) {
  * @return string                 The component description.
  */
 function get_component_description( $component_slug ) {
-  if ( ! class_exists( 'ParseDown' ) ) {
-    return false;
-  }
+	if ( ! class_exists( 'ParseDown' ) ) {
+		return false;
+	}
 
-  // Load the description text, if it exists.
-  $component_description_path = get_template_directory() . "/components/{$component_slug}/{$component_slug}-description.md";
+	// Load the description text, if it exists.
+	$component_description_path = get_template_directory() . "/components/{$component_slug}/{$component_slug}-description.md";
 
-  if ( ! file_exists( $component_description_path ) ) {
-    return false;
-  }
+	if ( ! file_exists( $component_description_path ) ) {
+		return false;
+	}
 
 	// Get the markdown file as a string.
 	$component_description_content = file_get_contents( $component_description_path );
 
 	// Parse the markdown to html.
-	$parsedown                = new \ParseDown();
-  $component_description = $parsedown->text( $component_description_content );
+	$parsedown             = new \ParseDown();
+	$component_description = $parsedown->text( $component_description_content );
 
-  return $component_description;
+	return $component_description;
 }
 
 /**
@@ -146,21 +155,21 @@ function get_component_description( $component_slug ) {
  * @return array                  The variations of the component.
  */
 function get_component_variations( $component_slug ) {
-  $component_variations = [];
+	$component_variations = [];
 
-  $component_variation_paths = glob( get_template_directory() . "/components/{$component_slug}/*.twig" );
+	$component_variation_paths = glob( get_template_directory() . "/components/{$component_slug}/*.twig" );
 
-  foreach ( $component_variation_paths as $key => $component_variation_path ) {
-    $component_variation_filename = basename( $component_variation_path );
+	foreach ( $component_variation_paths as $key => $component_variation_path ) {
+		$component_variation_filename = basename( $component_variation_path );
 
-    $component_variations[$component_variation_filename] = [
-      'filename' => $component_variation_filename,
-      'filepath' => "{$component_slug}/{$component_variation_filename}",
-      'data' => get_variation_data( $component_slug, $component_variation_filename ),
-    ];
-  }
+		$component_variations[ $component_variation_filename ] = [
+			'filename' => $component_variation_filename,
+			'filepath' => "{$component_slug}/{$component_variation_filename}",
+			'data'     => get_variation_data( $component_slug, $component_variation_filename ),
+		];
+	}
 
-  return $component_variations;
+	return $component_variations;
 }
 
 /**
@@ -171,15 +180,15 @@ function get_component_variations( $component_slug ) {
  * @return array                                The component variation data.
  */
 function get_variation_data( $component_slug, $component_variation_filename ) {
-  $variation_data_filename = str_replace( ".twig", "-data.json", $component_variation_filename );
-  $path_to_data_file = get_template_directory() . "/components/{$component_slug}/$variation_data_filename";
+	$variation_data_filename = str_replace( '.twig', '-data.json', $component_variation_filename );
+	$path_to_data_file       = get_template_directory() . "/components/{$component_slug}/$variation_data_filename";
 
-  if ( ! file_exists( $path_to_data_file ) ) {
-    return false;
-  }
+	if ( ! file_exists( $path_to_data_file ) ) {
+		return false;
+	}
 
-  $variation_contents = file_get_contents( $path_to_data_file );
-  $variation_data = json_decode( $variation_contents, true );
+	$variation_contents = file_get_contents( $path_to_data_file );
+	$variation_data     = json_decode( $variation_contents, true );
 
-  return $variation_data;
+	return $variation_data;
 }
