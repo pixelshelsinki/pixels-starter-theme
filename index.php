1977603d8e999a1469098da6c4bed78a79ee6d57
Thomas Hurd <thomas@pixels.fi>
2019-08-30 15:33:42 +0300

    fix index.php to pass tests

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php // silence is golden ?>
+<?php
+/**
+ * A blank index.php file.
+ *
+ * @package  WordPress
+ * @subpackage  PixelsTheme
+ */
+
+// This file is left blank on purpose, so that WordPress can see the theme and
+// display it in the theme chooser.
