c3f4a470816ad8a3702865bf472e8753ddcc3145
stscoundrel <sampo@pixels.fi>
2020-04-02 15:53:04 +0300

    Utils: add helper for breadcrumbs

--- lib/Utils/Common.php
+++ lib/Utils/Common.php
@@ -6,7 +6,17 @@
  * @subpackage  Pixels\Theme
  */
 
+
 namespace Pixels\Theme\Utils;
+
+// Breadcrumbs.
+use Pixels\Theme\Breadcrumbs\Trails\Archive;
+use Pixels\Theme\Breadcrumbs\Trails\Author;
+use Pixels\Theme\Breadcrumbs\Trails\Error404;
+use Pixels\Theme\Breadcrumbs\Trails\Page;
+use Pixels\Theme\Breadcrumbs\Trails\Post;
+use Pixels\Theme\Breadcrumbs\Trails\Search;
+use Pixels\Theme\Breadcrumbs\Trails\Taxonomy;
 
 /**
  * Common Utils class
@@ -31,5 +41,59 @@ class Common {
 		$types = get_post_types( $args, 'object' );
 
 		return $types;
+	}
+
+	/**
+	 * Return data array for breadcrumbs.
+	 *
+	 * @return array $breadcrumbs of page.
+	 */
+	public static function get_breadcrumbs() {
+
+		$breadcrumbs = array();
+
+		// Define breadcrumb type.
+		if ( is_singular() ) :
+
+			// Pages & singular posts.
+			if ( is_page() ) :
+				$handler = new Page();
+			else :
+				$handler = new Post();
+			endif;
+
+		else :
+
+			// Post type archives.
+			if ( is_archive() ) :
+				$handler = new Archive();
+			endif;
+
+			// Taxonomy terms.
+			if ( is_tax() ) :
+				$handler = new Taxonomy();
+			endif;
+
+			// Search
+			if ( is_search() ) :
+				$handler = new Search();
+			endif;
+
+			// Author
+			if ( is_author() ) :
+				$handler = new Author();
+			endif;
+
+			// 404.
+			if ( is_404() ) :
+				$handler = new Error404();
+			endif;
+
+		endif;
+
+		$breadcrumbs = $handler->get_breadcrumbs();
+
+		return $breadcrumbs;
+
 	}
 }
