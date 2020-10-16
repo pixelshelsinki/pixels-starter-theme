f0ee555f0e6c054ec1352e06e2e5bdc2673322eb
stscoundrel <sampo@pixels.fi>
2020-10-16 16:06:26 +0300

    Add cypress-axe

--- tests/Integration/config/support/commands.js
+++ tests/Integration/config/support/commands.js
@@ -7,19 +7,27 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+const a11yErrorLog = (violations) => {
+  cy.task(
+    'log',
+    `${violations.length} accessibility violation${
+      violations.length === 1 ? '' : 's'
+    } ${violations.length === 1 ? 'was' : 'were'} detected`,
+  )
+
+  const violationData = violations.map(
+    ({
+      id, impact, description, nodes,
+    }) => ({
+      id,
+      impact,
+      description,
+      nodes: nodes.length,
+    }),
+  )
+
+  cy.task('table', violationData)
+}
+
+Cypress.Commands.add('a11yErrorLog', a11yErrorLog)
