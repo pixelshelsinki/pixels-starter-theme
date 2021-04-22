afb1fa041689eda73cf65fa8f36896d431dbb93a
stscoundrel <sampo@pixels.fi>
2021-04-22 13:16:23 +0300

    SetupTests: fix lint

--- setupTests.js
+++ setupTests.js
@@ -4,32 +4,28 @@ import { Twig } from 'twig-testing-library'
 /**
  * Setup twig namespaces for include paths.
  */
-global.twigNamespaces = { 
-  'components': './views/components',
-  'layouts': './views/layouts',
+global.twigNamespaces = {
+  components: './views/components',
+  layouts: './views/layouts',
 }
 
 /**
  * Mock common translation functions.
  */
- Twig.extendFunction("__", (string, domain) => string)
+Twig.extendFunction('__', (string, domain) => string)
 
 /**
  * Mock responsive image functions, as they are PHP functions
  * outside of js-twig context.
  */
- Twig.extendFunction("responsive_image", function(id, mobileImage, desktopImage) {
-  return `printed image of id ${id} and sizes ${mobileImage} & ${desktopImage}`
-})
+Twig.extendFunction('responsive_image', (id, mobileImage, desktopImage) => `printed image of id ${id} and sizes ${mobileImage} & ${desktopImage}`)
 
-Twig.extendFunction("responsive_background", function(id, mobileImage, desktopImage, selector) {
-  return `printed background image of id ${id} and sizes ${mobileImage} & ${desktopImage} with selector ${selector}`
-})
+Twig.extendFunction('responsive_background', (id, mobileImage, desktopImage, selector) => `printed background image of id ${id} and sizes ${mobileImage} & ${desktopImage} with selector ${selector}`)
 
- /**
+/**
   * Mock social share function calls.
   */
-  Twig.extendFunction("facebook_share", () => 'https://facebook-share-link.dev')
-  Twig.extendFunction("twitter_share", () => 'https://twitter-share-link.dev')
-  Twig.extendFunction("linkedin_share", () => 'https://linkedin-share-link.dev')
-  Twig.extendFunction("whatsapp_share", () => 'https://whatsapp-share-link.dev')
+Twig.extendFunction('facebook_share', () => 'https://facebook-share-link.dev')
+Twig.extendFunction('twitter_share', () => 'https://twitter-share-link.dev')
+Twig.extendFunction('linkedin_share', () => 'https://linkedin-share-link.dev')
+Twig.extendFunction('whatsapp_share', () => 'https://whatsapp-share-link.dev')
